7797ffd255af76e1290329782f1ba81feb554f85
Vaibhav200303 <vs9109807@gmail.com>
2024-11-08 21:21:48 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:nb_utils/nb_utils.dart';
+import 'package:quiz/Screens/quiz_splash_screen.dart';
+
+
+void main() async {
+  // await initialize();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Quiz App',
+      home:const QuizSplashScreen(),
+      debugShowCheckedModeBanner: false,
+      navigatorKey: navigatorKey,
+    );
+  }
+}
